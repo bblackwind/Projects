8a6efc4ee92ff9f075196fd289e5ae74d153ad45
bblackwind <vishal230396@gmail.com>
2025-05-06 17:18:37 +0530

    modal creation started

--- Jira/script.js
+++ Jira/script.js
@@ -1,12 +1,36 @@
 let allfilters = document.querySelectorAll(".filter div");
 let grid = document.querySelector(".grid");
-
+let modalVisible = false;
+let body = document.querySelector("body");
+let addBtn = document.querySelector(".add");
 let colors = {
     pink:"#d595aa",
     blue:"#5ecdde",
     green:"#91e6c7",
     black:"black"
 }
+
+addBtn.addEventListener("click",function(){
+    if(modalVisible){
+        return;
+    }
+
+    let modal = document.createElement("div");
+    modal.classList.add("modal-container");
+
+    modal.innerHTML = ` 
+        <div class="writing-area" contenteditable=""></div>
+            <div class="filter-area">
+                <div class="modal-filter pink"></div>
+                <div class="modal-filter blue"></div>
+                <div class="modal-filter green"></div>
+                <div class="modal-filter black"></div>
+            </div>`;
+    body.appendChild(modal);
+    modalVisible = true;
+
+});
+
 
 
 
